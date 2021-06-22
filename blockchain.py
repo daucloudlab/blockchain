c0ee990ad2df96b2cd9b9ce435ee1dbeff15f72b
Daulet Baimbetov <daucloudlab@gmail.com>
2021-06-22 08:49:20 +0600

    Бірнеше транзакцияны енгізуге болады

--- blockchain.py
+++ blockchain.py
@@ -1,19 +1,46 @@
 blockchain = []
 
 def get_last_blockchain_value():
+    if len(blockchain) < 1:
+        return None
     return blockchain[-1]
 
-def add_value(transaction_ammount, last_transaction = [1]):
+def add_transactions(transaction_ammount, last_transaction = [1]):
+    if last_transaction is None:
+        last_transaction = [1]
     blockchain.append([last_transaction, transaction_ammount])
 
-def get_user_input():
+def get_transaction_value():
     user_input = float(input('Your transaction amount please... '))
     return user_input
 
-tx_amount = get_user_input()
-add_value(tx_amount)
+def get_user_choice():
+    user_choice = input()
+    return user_choice
 
-tx_amount = get_user_input()
-add_value(tx_amount, get_last_blockchain_value())
+def print_blockchain_elements():
+    for block in blockchain:
+        print("Output a block")
+        print(block)
 
-print(blockchain)
+
+while True:
+    print("\nPlease, choose:")
+    print('1: Add a new transaction value')
+    print('2: Output the blockchain blocks')
+    print('q: Quit')
+    user_choice = get_user_choice()
+    if user_choice == '1':
+        tx_amount = get_transaction_value()
+        add_transactions(tx_amount, get_last_blockchain_value())
+        print("Transaction added successfully!")
+        print("\n")
+    elif user_choice == '2':
+        print_blockchain_elements()
+    elif user_choice == 'q':
+        break    
+    else:
+        print("\n!!!Please, choose correct number!!!")
+        continue
+    
+print("Done!")
